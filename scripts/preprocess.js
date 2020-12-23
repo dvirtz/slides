1c938ca747e78467eea18ee779fc42cdc2bfa4f1
dvirtz <dvirtz@gmail.com>
2020-12-23 20:09:35 +0200

    add random

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -1,13 +1,63 @@
-const SLIDES = /^# Slides/
-const FILE_REF = /^FILE: (.+)$/gm;
+const { readFileSync } = require('fs');
+const path = require('path');
+const { getInitialDir } = require('reveal-md/lib/config');
+
+const LINE_SEPARATOR = '\n';
+const FILE_REF_REGEX = /^FILE: (.+)$/;
+const SVG_START = /<svg.*>/;
+const SVG_END = /<\/svg>/;
+const SVG_ID = /id="([^"]+)"/;
+const SVG_URL = /url\(#([^)]+)\)/;
+const SLIDES = /^# Slides/;
 const SLIDE_REF = /\.md\)/m;
 
-module.exports = async (markdown, options) => {
+const loadFileContent = (filePath, options, svgState) => {
+  const contents = readFileSync(path.join(options.initialDir, filePath), 'utf-8');
+  if (path.extname(filePath) === '.md') {
+    return preprocessMarkdown(contents, options, svgState);
+  }
+  return contents;
+};
+
+const processLine = (line, options, svgState) => {
+  for (const m of (line.match(FILE_REF_REGEX) || []).slice(1)) {
+    return loadFileContent(m, options, svgState);
+  }
+
+  if (svgState.insideSvg) {
+    if (line.match(SVG_END)) {
+      svgState.insideSvg = false;
+      svgState.svgCounter = svgState.svgCounter + 1;
+    } else {
+      return line.replace(SVG_ID, `id="$1${svgState.svgCounter}"`).replace(SVG_URL, `url(#$1${svgState.svgCounter})`);
+    }
+  } else if (line.match(SVG_START)) {
+    svgState.insideSvg = true;
+  }
+
+  return line;
+}
+
+const preprocessMarkdown = (markdown, options, svgState) => {
+  return markdown
+    .split(LINE_SEPARATOR)
+    .map(line => processLine(line, options, svgState))
+    .join(LINE_SEPARATOR);
+}
+
+const preprocess = async (markdown, options) => {
+  var svgState = {
+    insideSvg: false,
+    svgCounter: 0
+  };
+
   if (markdown.match(SLIDES) && options.static) {
     return markdown.replace(SLIDE_REF, '.html)');
   }
 
-  return markdown.replace(FILE_REF, function (p1) {
-    return readFile(join(options.includeDir, p1));
-  });
+  options.initialDir = await getInitialDir();
+  let res = preprocessMarkdown(markdown, options, svgState);
+  return res;
 }
+
+module.exports = preprocess;
--- test/test_code_snippets.js
+++ test/test_code_snippets.js
@@ -1,124 +1,28 @@
-const { processElement, directives } = require('../scripts/compiler-explorer');
-const { readFileSync } = require('fs');
-const { describe, it } = require('mocha');
-const bent = require('bent');
-const chai = require('chai')
-, should = chai.should();
-const { unstyle } = require('ansi-colors');
-const { join } = require('path');
+const { parseMarkdownSync, compile } = require('reveal-test');
+const assert = require('assert');
+const path = require('path');
 const { cwd } = require('process');
-const { listFiles } = require('reveal-md/lib/util');
-const promiseRetry = require('promise-retry');
+const FileHound = require('filehound');
 
-const SNIPPET_MARK = /^\s*```/;
-const compile = bent('https://godbolt.org/', 'POST', 'json');
-
-const fileSnippets = file => {
-  let snippets = [];
-  let currentSnippet = null;
-
-  readFileSync(join('slides', file), 'ascii')
-    .split('\n')
-    .forEach((line, index) => {
-      if (currentSnippet) {
-        if (line.match(SNIPPET_MARK)) {
-          snippets.push(currentSnippet);
-          currentSnippet = null
-        } else {
-          currentSnippet.code.push(line)
-        }
-      } else if (line.match(SNIPPET_MARK)) {
-        currentSnippet = {
-          shortLocation: `${file}:${index + 1}`,
-          fullLocation: `${join(cwd(), 'slides', file)}:${index + 1}`,
-          code: []
-        }
-      }
-    });
-
-  return snippets;
+const config = {
+  compiler: 'g83'
 };
 
-listFiles('slides', '**/*.md')
+FileHound.create().path('slides').ext('md').findSync()
   .map(file => {
     return {
-      file: file,
-      snippets: fileSnippets(file)
+      file: path.relative('slides', file),
+      snippets: parseMarkdownSync(path.join(cwd(), file), config)
     };
   })
   .filter(fileSnippets => fileSnippets.snippets.length > 0)
   .map(fileSnippets => {
-    describe(`compile ${fileSnippets.file}`, function () {
-      this.timeout(60000);
+    describe(`compile snippets from ${fileSnippets.file}`, function () {
 
-      fileSnippets.snippets.map(function (codeSnippet, index) {
-        it(`should compile ${fileSnippets.file} snippet #${index}`, async function () {
-          for (const line of codeSnippet.code) {
-            const directiveMessage = `${codeSnippet.fullLocation} :\nwrong directive: ${line}`
-            if (line.startsWith('//') && !line.startsWith('///')) {
-              for (directive of directives) {
-                line.should.not.match(directive, directiveMessage);
-              };
-            }
-            else if (line.startsWith('///')) {
-              line.should.satisfy(line => directives.some(directive => line.match(directive)), directiveMessage);
-            }
-          }
-          const [info] = processElement(codeSnippet.code.join(['\n']))
-          const data = {
-            source: info.source,
-            options: {
-              userArguments: info.options,
-              filters: {
-                execute: info.execute
-              },
-              libraries: info.libs.map(function (lib) {
-                return {
-                  id: lib.name,
-                  version: lib.ver
-                };
-              })
-            }
-          };
-          const response = await promiseRetry(async (retry) => {
-            try {
-              return compile(`api/compiler/${info.compiler}/compile`, data);
-            }
-            catch (err) {
-              if (err.statusCode / 100 == 5) {
-                retry(err);
-              }
-
-              throw err;
-            }
-          });
-          const error = (stderr) => unstyle(stderr.map(x => x.text).join('\n'));
-          const message = (error) => `\n${codeSnippet.fullLocation} :\n${error}`;
-          const compileError = error(response.stderr);
-          const compileMessage = message(compileError);
-          if (info.execute) {
-            const execError = error(response.execResult.buildResult.stderr.concat(response.execResult.stderr));
-            const execMessage = message(execError);
-            if (info.failReason) {
-              if (response.code === 0) {
-                response.execResult.code.should.not.equal(0, execMessage);
-                execError.should.contain(info.failReason, execMessage);
-              } else {
-                compileError.should.contain(info.failReason)
-              }
-            }
-            else {
-              response.execResult.code.should.be.at.least(0, execMessage);
-            }
-          }
-          else if (info.failReason) {
-            response.code.should.not.equal(0, compileMessage);
-            compileError.should.contain(info.failReason, compileMessage);
-          } else {
-            response.code.should.equal(0, compileMessage);
-          }
+      fileSnippets.snippets.forEach(function (codeSnippet, index) {
+        it(`should compile snippet #${index} of ${fileSnippets.file}`, async function () {
+          await compile(codeSnippet);
         });
       });
-
     });
   });
