ecfbf8728d1886aa53448831ae2474b9e3a5bb95
dvirtz <dvirtz@gmail.com>
2021-06-23 20:54:17 +0300

    upgrade deps

--- test/test_code_snippets.js
+++ test/test_code_snippets.js
@@ -1,28 +1,29 @@
-const { parseMarkdownSync, compile } = require('reveal-test');
-const assert = require('assert');
-const path = require('path');
-const { cwd } = require('process');
-const FileHound = require('filehound');
+import { parseMarkdown, compile } from 'reveal-test';
+import path from 'path';
+import { cwd } from 'process';
+import FileHound from 'filehound';
 
 const config = {
   compiler: 'g83'
 };
 
-FileHound.create().path('slides').ext('md').findSync()
-  .map(file => {
+const snippets = await FileHound.create().path('slides').ext('md').find()
+  .map(async file => {
     return {
       file: path.relative('slides', file),
-      snippets: parseMarkdownSync(path.join(cwd(), file), config)
+      snippets: await parseMarkdown(path.join(cwd(), file), config)
     };
   })
-  .filter(fileSnippets => fileSnippets.snippets.length > 0)
-  .map(fileSnippets => {
-    describe(`compile snippets from ${fileSnippets.file}`, function () {
+  .filter(fileSnippets => fileSnippets.snippets.length > 0);
 
-      fileSnippets.snippets.forEach(function (codeSnippet, index) {
-        it(`should compile snippet #${index} of ${fileSnippets.file}`, async function () {
-          await compile(codeSnippet);
-        });
+
+snippets.map(fileSnippets => {
+  describe(`compile snippets from ${fileSnippets.file}`, function () {
+
+    fileSnippets.snippets.forEach(function (codeSnippet, index) {
+      it(`should compile snippet #${index} of ${fileSnippets.file}`, async function () {
+        await compile(codeSnippet);
       });
     });
   });
+});
